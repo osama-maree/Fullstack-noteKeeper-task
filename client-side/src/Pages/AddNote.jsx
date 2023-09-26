d65644eed4512fc7afb8f5ed15a757d87334dceb
osama maree <osamamarei2001@gmail.com>
2023-09-27 00:45:52 +0300

    Add Search Item

--- client-side/src/Pages/AddNote.jsx
+++ client-side/src/Pages/AddNote.jsx
@@ -1,5 +1,12 @@
-import React from "react";
-import { TextField, Button, Grid, Typography, Container } from "@mui/material";
+import React, { useState } from "react";
+import {
+  TextField,
+  Button,
+  Grid,
+  Typography,
+  Container,
+  Alert,
+} from "@mui/material";
 import { useFormik } from "formik";
 import * as yup from "yup";
 
@@ -7,21 +14,47 @@ const validationSchema = yup.object({
   title: yup.string().min(5).required("Title is required"),
   content: yup.string().min(5).required("Content is required"),
 });
-
+const initialValues = {
+  title: "",
+  content: "",
+};
 function AddNote() {
+  const [success, setSuccess] = useState(false);
   const formik = useFormik({
-    initialValues: {
-      title: "",
-      content: "",
-    },
+    initialValues,
     validationSchema: validationSchema,
-    onSubmit: (values) => {
-      console.log(values);
+    onSubmit: ({ title, content }) => {
+      addNote(title, content);
+      formik.resetForm(initialValues);
     },
   });
-
+  const addNote = (title, content) => {
+    fetch("http://localhost:3001/api/notes/v1", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ title, content }),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((responseData) => {
+        setSuccess(true);
+        console.log("Response Data:", responseData);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
   return (
     <Container maxWidth="md">
+      {success && (
+        <Alert severity="success">Success Added Note — check it!</Alert>
+      )}
       <form onSubmit={formik.handleSubmit}>
         <Typography
           variant="h4"
