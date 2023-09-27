e2b005c0e4075996ffc60909e85b6a777d6805cc
osama maree <osamamarei2001@gmail.com>
2023-09-27 10:34:35 +0300

    Make Content Is Dynamic

--- client-side/src/component/Profile.jsx
+++ client-side/src/component/Profile.jsx
@@ -1,12 +1,12 @@
 import { Avatar, Typography } from "@mui/material";
 import React from "react";
 
-const Profile = () => {
+const Profile = ({ img, title }) => {
   return (
     <>
-      <Avatar alt="Remy Sharp" src="/assets/myImg.png" />
+      <Avatar alt="Remy Sharp" src={`/assets/${img}`} />
       <Typography variant="body1" sx={{ color: "#78909c !important" }} ml={2}>
-        Osama Maree
+        {title}
       </Typography>
     </>
   );
